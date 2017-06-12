ad43e4dff23134f0c956d9c13eaf70c65478dfbf
Rasmus Fuhse <fuhse@data-quest.de>
2017-06-12 18:15:29 +0200

    work on

--- controllers/oauth.php
+++ controllers/oauth.php
@@ -42,21 +42,6 @@ class OauthController extends PluginController
                 . "&redirect_uri=".urlencode($redirect_uri)
                 . "&client_id=".urlencode($client_id);
 
-
-        $provider = new \League\OAuth2\Client\Provider\GenericProvider([
-            'clientId'                => $client_id,    // The client ID assigned to you by the provider
-            'clientSecret'            => Config::get()->OWNCLOUD_CLIENT_SECRET ?: UserConfig::get($GLOBALS['user']->id)->OWNCLOUD_CLIENT_SECRET,   // The client password assigned to you by the provider
-            'redirectUri'             => $redirect_uri,
-            'urlAuthorize'            => $owncloud."index.php/apps/oauth2/authorize",
-            'urlAccessToken'          => $owncloud."index.php/apps/oauth2/api/v1/token",
-            'urlResourceOwnerDetails' => $owncloud."index.php/apps/oauth2/resource"
-        ]);
-        $authorizationUrl = $provider->getAuthorizationUrl();
-
-        //$_SESSION['oauth2state'] = $provider->getState();
-
-        //die($authorizationUrl);
-
         header("Location: ".$url);
         $this->render_nothing();
     }
@@ -102,11 +87,8 @@ class OauthController extends PluginController
             die();
             throw new AccessDeniedException();
         }
+        //unset $_SESSION['oauth2state'];
 
-
-        $header = array();
-
-        $header[] = "Accept: application/json";
 
         $client_id  = \Config::get()->OWNCLOUD_CLIENT_ID ?: \UserConfig::get($GLOBALS['user']->id)->OWNCLOUD_CLIENT_ID;
         $client_secret = \Config::get()->OWNCLOUD_CLIENT_SECRET ?: \UserConfig::get($GLOBALS['user']->id)->OWNCLOUD_CLIENT_SECRET;
@@ -117,19 +99,25 @@ class OauthController extends PluginController
             'grant_type' => "authorization_code",
             'code' => Request::get("code"),
             'redirect_uri' => $redirect_uri,
-            'client_id' => $client_id,    // The client ID assigned to you by the provider
-            'client_secret' => $client_secret,   // The client password assigned to you by the provider
-            'format' => "json"
+            //'client_id' => $client_id,    // The client ID assigned to you by the provider
+            //'client_secret' => $client_secret,   // The client password assigned to you by the provider
+            //'format' => "json"
+
         );
 
+        $header = array();
+        $header[] = "Accept: application/json";
+        $header[] = "Authorization: Basic ".base64_encode($client_id . ":" .$client_secret);
+
         $r = curl_init();
-        curl_setopt($r, CURLOPT_URL, $owncloud."index.php/apps/oauth2/authorize?grant_type=authorization_code&code=".urlencode(Request::get("code"))."&redirect_uri=".urlencode($redirect_uri));
-        curl_setopt($r, CURLOPT_USERPWD, $client_id . ":" . $client_secret);
-        //curl_setopt($r, CURLOPT_POST, 1);
+        //curl_setopt($r, CURLOPT_URL, $owncloud."index.php/apps/oauth2/authorize?grant_type=authorization_code&code=".urlencode(Request::get("code"))."&redirect_uri=".urlencode($redirect_uri));
+        curl_setopt($r, CURLOPT_URL, $owncloud."index.php/apps/oauth2/authorize");
+        //curl_setopt($r, CURLOPT_USERPWD, $client_id . ":" . $client_secret);
+        curl_setopt($r, CURLOPT_POST, 1);
         curl_setopt($r, CURLOPT_HTTPHEADER, studip_utf8encode($header));
         curl_setopt($r, CURLOPT_RETURNTRANSFER, 1);
 
-        //curl_setopt($r, CURLOPT_POSTFIELDS, studip_utf8encode($payload));
+        curl_setopt($r, CURLOPT_POSTFIELDS, studip_utf8encode($payload));
 
         $result = curl_exec($r);
         $curl_info = curl_getinfo($r);
