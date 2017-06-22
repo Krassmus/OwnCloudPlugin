be5966c78d6fcf9526e8a8ec65c967178f401e3d
Rasmus Fuhse <fuhse@data-quest.de>
2017-06-22 12:22:38 +0200

    debugging

--- OwnCloudPlugin.class.php
+++ OwnCloudPlugin.class.php
@@ -31,9 +31,13 @@ class OwnCloudPlugin extends StudIPPlugin implements FilesystemPlugin {
         $client = new \Sabre\DAV\Client(array(
             'baseUri' => $webdav
         ));
+        //$client->addCurlSetting();
+        $client->on("beforeRequest", function ($request) {
+            var_dump($request);
+        });
 
         $response = $client->request('GET', "", null, array(
-            "Authorization:Bearer ".\Owncloud\OAuth::getAccessToken()
+            "Authorization: Bearer ".\Owncloud\OAuth::getAccessToken()
         ));
 
         var_dump($response);die();
