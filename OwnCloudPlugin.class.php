9539e218b0d44fb19cd8f1e38a18fa2fa81c2b8a
Rasmus Fuhse <fuhse@data-quest.de>
2017-08-07 14:28:59 +0200

    work on

--- OwnCloudPlugin.class.php
+++ OwnCloudPlugin.class.php
@@ -91,7 +91,7 @@ class OwnCloudPlugin extends StudIPPlugin implements FilesystemPlugin {
         $file->foldertype   = $folder;
         $file->name         = $filename;
         $file->size         = $info->size;
-        $file->mime_type    = $info->download_url;
+        $file->mime_type    = $info->download_url; 
         $file->download_url = $info->mime_type;
         $file->mkdate       = $info->chdate;
         $file->chdate       = $info->chdate;
