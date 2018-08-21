bbb9234292d654aaa2ed1a5095be1c6ea764a1d3
Rasmus Fuhse <fuhse@data-quest.de>
2018-08-21 16:58:08 +0200

    fix configs of 4.1

--- migrations/1_init_plugin.php
+++ migrations/1_init_plugin.php
@@ -1,9 +1,11 @@
 <?php
 
-class InitPlugin extends Migration {
-    public function up() {
+class InitPlugin extends Migration
+{
+    public function up()
+    {
         $configs =     array("OWNCLOUD_ENDPOINT", "OWNCLOUD_CLIENT_ID", "OWNCLOUD_CLIENT_SECRET");
-        $userconfigs = array("OWNCLOUD_ENDPOINT", "OWNCLOUD_CLIENT_ID", "OWNCLOUD_CLIENT_SECRET", "OWNCLOUD_ACTIVATED", "OWNCLOUD_ACCESS_TOKEN", "OWNCLOUD_ACCESS_TOKEN_EXPIRES", "OWNCLOUD_REFRESH_TOKEN");
+        $userconfigs = array("OWNCLOUD_ENDPOINT_USER", "OWNCLOUD_CLIENT_ID_USER", "OWNCLOUD_CLIENT_SECRET_USER", "OWNCLOUD_ACTIVATED", "OWNCLOUD_ACCESS_TOKEN", "OWNCLOUD_ACCESS_TOKEN_EXPIRES", "OWNCLOUD_REFRESH_TOKEN");
         foreach ($configs as $config) {
             Config::get()->create($config, array(
                 'value' => "",
@@ -13,12 +15,24 @@ class InitPlugin extends Migration {
             ));
         }
         foreach ($userconfigs as $config) {
-            UserConfig::get()->create($config."_USER", array(
+            Config::get()->create($config, array(
                 'value' => "",
                 'type' => in_array($config, ["OWNCLOUD_ACTIVATED"]) ? "boolean" : "string",
                 'range' => "user",
                 'section' => "Owncloud"
             ));
+        }
+    }
+
+    public function down()
+    {
+        $configs =     array("OWNCLOUD_ENDPOINT", "OWNCLOUD_CLIENT_ID", "OWNCLOUD_CLIENT_SECRET");
+        $userconfigs = array("OWNCLOUD_ENDPOINT_USER", "OWNCLOUD_CLIENT_ID_USER", "OWNCLOUD_CLIENT_SECRET_USER", "OWNCLOUD_ACTIVATED", "OWNCLOUD_ACCESS_TOKEN", "OWNCLOUD_ACCESS_TOKEN_EXPIRES", "OWNCLOUD_REFRESH_TOKEN");
+        foreach ($configs as $config) {
+            Config::get()->delete($config);
+        }
+        foreach ($userconfigs as $config) {
+            Config::get()->delete($config);
         }
     }
 }
